127f8bd2564f8f1988d3bfd94a3f7d92fbbeab03
pastme <pacufist@gmail.com>
2014-11-11 16:54:40 +0200

    ticket:1 Edited tests

--- apps/contacts/tests.py
+++ apps/contacts/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.template.defaultfilters import escape, date
+from django.template.defaultfilters import escape, date, linebreaks
 
 from contacts.models import Contacts
 
@@ -7,15 +7,15 @@ class ContactTest(TestCase):
     fixtures = ['initial_data.json']
 
     def test_contact(self):
-        response = self.client.get('/contacts/1/')
+        response = self.client.get('')
         contact = Contacts.objects.get(pk=1)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, contact.name)
         self.assertContains(response, contact.surname)
-        self.assertContains(response, escape(contact.bio))
+        self.assertContains(response, linebreaks(escape(contact.bio)))
         self.assertContains(response, date(contact.date_of_birth))
         self.assertContains(response, contact.email)
         self.assertContains(response, contact.skype)
         self.assertContains(response, contact.jabber)
-        self.assertContains(response, escape(contact.contacts))
+        self.assertContains(response, linebreaks(escape(contact.contacts)))
 
