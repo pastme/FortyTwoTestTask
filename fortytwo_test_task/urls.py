4b214490b3ba389ef29084ee9bbd5518d2dc7a6f
pastme <pacufist@gmail.com>
2014-11-12 16:06:48 +0200

    ticket5:changed urls

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -15,3 +16,4 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
